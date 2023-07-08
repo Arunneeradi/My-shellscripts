2979d37fe8279798098035f628928f1fe0654e3e
root <root@ip-10-0-1-65.ec2.internal>
2023-07-08 12:35:35 +0000

    variable01.sh

---
+++ variable01.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "hi good morning ${PERSON1}"
+echo "hi ${PERSON2} wish u a good mrng"
---
+++ virable.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PERSON1=kishore
+PERSON2=arun
+echo "hi good morning ${PERSON1}"
+echo "hi ${PERSON2} wish u a good mrng"
